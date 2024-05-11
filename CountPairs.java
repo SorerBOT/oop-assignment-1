1b20c462371f97893342a1b778e1e666c5fb6989
SorerBOT <alonfiller@gmail.com>
2024-05-11 20:23:34 +0300

    (Patch) SumOfInteger empty args handling

---
+++ CountPairs.java
@@ -0,0 +1,5 @@
+public class CountPairs {
+    public static void main(String[] args) {
+        
+    }
+}
--- SumOfInteger.java
+++ SumOfInteger.java
@@ -1,5 +1,9 @@
 public class SumOfInteger {
     public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Invalid input");
+            return;
+        }
         System.out.println(findSumOfIntegers(Integer.valueOf(args[args.length - 1])));
     }
     public static int findSumOfIntegers(int number) {
